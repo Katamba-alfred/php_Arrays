5cc67e5d01daacc47a669c570076106d1d20c6e1
Katamba-alfred <katambaalfred57@gmail.com>
2023-02-07 15:47:40 -0800

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -13,9 +13,14 @@
 $band[] = 'Katamba';
 $band[] = 'Alfred';
 $band[] = 'Murray';
+$band['LL'] = 'La Liga';
+$band['LL'] = 'La Liga-Santander';
+$band['EPL'] = 'Premier League';
+$band['EPL'] = 'English Premier League';
+echo "<p>His name is $band[0] and his last name is $band[2]</p>.
+<p>{$band['LL']} and {$band['EPL']}.</p>";
 
-
-echo "His name is $band[0] and his last name is $band[2]"; 
+//The program will print English premier league and la liga santander instead of La liga and Premier league
 ?>
 
 </body>
