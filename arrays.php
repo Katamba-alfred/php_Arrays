c3987b461bcdd448e53809c0a7ee0af7f56777d5
Katamba-alfred <katambaalfred57@gmail.com>
2023-02-07 15:35:34 -0800

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Arrays</title>
+
+</head>
+<body>
+
+<h1>Some simple Arrays</h1>
+<?php
+$band[] = 'Katamba';
+$band[] = 'Alfred';
+$band[] = 'Murray';
+
+
+echo "His name is $band[0] and his last name is $band[2]"; 
+?>
+
+</body>
+</html>
